680d00ba9a57a412bc73483506de6d447423800c
이상호 <sangholee@isanghoui-MacBookAir.local>
2025-05-24 09:38:51 +0900

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/nextjs_test',
+}
 
 module.exports = nextConfig
