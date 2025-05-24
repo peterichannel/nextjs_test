1f0976f34d9ff4716f6eb7edb025c369afa8064c
이상호 <sangholee@isanghoui-MacBookAir.local>
2025-05-24 10:20:21 +0900

    Fix image-loader.js formatting

--- image-loader.js
+++ image-loader.js
@@ -1,1 +1,5 @@
-export default function customImageLoader({ src }) { return process.env.NODE_ENV === "production" ? `https://peterichannel.github.io/nextjs_test${src}` : src; }
+export default function customImageLoader({ src }) {
+  return process.env.NODE_ENV === 'production'
+    ? `https://peterichannel.github.io/nextjs_test${src}`
+    : src;
+}
